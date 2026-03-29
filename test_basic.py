388e1038e3dfc7d4fb7aaa7b6e0dda7978c6c4a3
samridh <mrunknown555x7007@gmail.com>
2026-03-29 17:38:37 +0530

    initial commit

---
+++ test_basic.py
@@ -0,0 +1,16 @@
+AI Demonstration Started. Playing as Black.
+Image saved to start_pos.png
+White played: 1. e4
+Image saved to turn_1_e4.png
+Image saved to white_move_2.png
+Image saved to black_move_2.png
+Image saved to white_move_3.png
+Image saved to black_move_3.png
+Image saved to white_move_4.png
+Image saved to black_move_4.png
+Image saved to white_move_5.png
+Image saved to black_move_5.png
+Image saved to white_move_6.png
+Image saved to black_move_6.png
+Image saved to checkmate.png
+Checkmate! White wins.
